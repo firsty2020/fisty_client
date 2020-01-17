c9f7039605f253b3a359c9de6089d9ec5e3d65aa
varo <varazdat.manukyan@click2sure.co.za>
2020-01-17 17:12:17 +0400

    registration form impelemented

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(() => {})}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
