0af6ea6460c275a8e901140ec97249494e4de681
firsty2020 <varazdat.manukyan@click2sure.co.za>
2020-02-14 15:10:15 +0400

    lazy load admin component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Suspense, lazy} from 'react';
 import {
     Registration,
     SetPassword,
     Recruiter,
     Landing,
     Login,
-    Admin,
 } from './components';
 import { Switch, Route } from 'react-router-dom';
 import './assets/vibe/scss/styles.scss';
 import './App.css';
 
+const Admin = lazy(() => import('./components/admin/Dashboard'));
 
 function App() {
     return (
         <div className="App">
-            <Switch>
-                <Route path="/register" component={Registration}/>
-                <Route path="/sign-up/:passwordToken" component={SetPassword}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/recruiter" component={Recruiter}/>
-                <Route path="/admin" component={Admin}/>
-                <Route path="/" component={Landing}/>
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/register" component={Registration}/>
+                    <Route path="/sign-up/:passwordToken" component={SetPassword}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/recruiter" component={Recruiter}/>
+                    <Route path="/admin" component={Admin}/>
+                    <Route path="/" component={Landing}/>
+                </Switch>
+            </Suspense>
         </div>
     );
 }
