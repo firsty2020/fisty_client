8ccfb5b0ffa87ef647cb8059651a3e96f664e89a
Varo Manukyan <varomanukyan@gmail.com>
2020-06-10 21:58:52 +0400

    implement email and phone search

--- src/components/admin/User/Filters.js
+++ src/components/admin/User/Filters.js
@@ -46,14 +46,14 @@ const Filters = ({ onFilter }) => {
         onFilter(_filter);
     };
 
-    const handleCityFilter = (e) => {
+    const handleSearch = (e, query) => {
         e.persist();
         const value = e.target.value;
         let _filter = { ...filter };
         if (value) {
-            _filter = { ..._filter, city: value };
+            _filter = { [query]: value };
         } else {
-            delete _filter.city;
+            delete _filter[query];
         }
         setFilter(_filter);
         onFilter(_filter);
@@ -63,52 +63,61 @@ const Filters = ({ onFilter }) => {
         <div>
             <Accordion>
                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    <Filter />
+                    <span title="Фильтровать"><Filter/></span>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="0">
-                    <Row>
-                        <Col>
-                            <DropDown
-                                isClearable
-                                name="status"
-                                placeholder="Статус"
-                                onChange={(e) => handleFilterChange(e,'status')}
-                                options={statusOptions}
-                            />
-                        </Col>
-                        <Col>
-                            <DropDown
-                                isClearable
-                                name="role"
-                                placeholder="Роль"
-                                onChange={(e) => handleFilterChange(e, 'role')}
-                                options={roleOptions}
-                            />
-                        </Col>
-                        <Col>
-                            <DropDown
-                                isClearable
-                                name="sub_role"
-                                placeholder="Тип"
-                                onChange={(e) => handleFilterChange(e, 'sub_role')}
-                                options={subRoleOptions}
-                            />
-                        </Col>
-                        <Col>
-                            <DropDown
-                                isClearable
-                                name="citizenship"
-                                placeholder="Гражданство"
-                                onChange={(e) => handleFilterChange(e, 'citizenship')}
-                                options={countriesOptions}
-                            />
-                        </Col>
-                        <Col>
-                            <Input
-                                onChange={(e) => handleCityFilter(e)}
-                                placeholder="Город"/>
-                        </Col>
-                    </Row>
+                    <div>
+                        <Row>
+                            <Col>
+                                <DropDown
+                                    isClearable
+                                    name="status"
+                                    placeholder="Статус"
+                                    onChange={(e) => handleFilterChange(e,'status')}
+                                    options={statusOptions}
+                                />
+                            </Col>
+                            <Col>
+                                <DropDown
+                                    isClearable
+                                    name="role"
+                                    placeholder="Роль"
+                                    onChange={(e) => handleFilterChange(e, 'role')}
+                                    options={roleOptions}
+                                />
+                            </Col>
+                            <Col>
+                                <DropDown
+                                    isClearable
+                                    name="sub_role"
+                                    placeholder="Тип"
+                                    onChange={(e) => handleFilterChange(e, 'sub_role')}
+                                    options={subRoleOptions}
+                                />
+                            </Col>
+                            <Col>
+                                <DropDown
+                                    isClearable
+                                    name="citizenship"
+                                    placeholder="Гражданство"
+                                    onChange={(e) => handleFilterChange(e, 'citizenship')}
+                                    options={countriesOptions}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="mt-1">
+                            <Col>
+                                <Input
+                                    onChange={(e) => handleSearch(e, 'city__icontains')}
+                                    placeholder="Город"/>
+                            </Col>
+                            <Col>
+                                <Input
+                                    onChange={(e) => handleSearch(e, 'search')}
+                                    placeholder="Телефон, эл.почта"/>
+                            </Col>
+                        </Row>
+                    </div>
                 </Accordion.Collapse>
             </Accordion>
         </div>
